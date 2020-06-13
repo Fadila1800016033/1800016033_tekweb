67f11042fb9fa5e3abd6d19b628b1c7b9ca9c764
Fadila1800016033 <fadila1800016033@webmail.uad.ac.id>
2020-06-13 18:51:42 +0700

    Teknologi Web 1800016033

---
+++ mhs.php
@@ -0,0 +1,37 @@
+<?php
+echo "<right>";
+echo "<table border>";
+
+    echo "</tr>";
+    echo "<td> NIM :</td>";
+    echo "<td>".$_POST['NIM']."</td>";
+    echo "</tr>";
+    echo "<tr>";
+    echo "<td> Nama :</td>";
+    echo "<td>".$_POST['NAMA']."</td>";
+    echo "</tr>";
+    echo "<tr>";
+    echo "<td> Jenis Kelamin :</td>";
+    echo "<td>".$_POST['KELAMIN']."</td>";
+    echo "</tr>";
+    echo "<tr>";
+    echo "<td> Alamat :</td>";
+    echo "<td>".$_POST['ALAMAT']."</td>";
+    echo "</tr>";
+    echo "<tr>";
+    echo "<td> Program Studi :</td>";
+    echo "<td>".$_POST['PRODI']."</td>";
+    echo "</tr>";
+    echo "<tr>";
+    echo "<td> Bahasa Pemrograman yang di Kuasai :</td>";
+    echo "<td>".$_POST['BP']."</td>";
+    echo "</tr>";
+    echo "<tr>";
+
+echo "<table>";
+echo "</right>";
+
+?>
+
+</body>
+</html>
